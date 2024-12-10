25f00786a39e080a51b0258c47272d8b88efbeae
ridhorz <idhoo.rz@gmail.com>
2024-12-10 17:15:53 +0700

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	import type { Config } from '@sveltejs/adapter-vercel';
+
+	export const config: Config = {
+		runtime: 'nodejs22.x'
+	};
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import '../app.css';
